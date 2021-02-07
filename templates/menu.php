a0f6649eff20c1f8e5b14aef90384d4b3dca29c9
Nikolay S <nikolay1002@yandex.ru>
2021-02-07 19:06:43 +0300

    masta

--- templates/menu.php
+++ templates/menu.php
@@ -1,13 +1,5 @@
 <ul>
-<?php
-foreach ($menu as $key => $value) {
-?>
+<?php foreach ($menu as $key => $value):?>
 <li><a href="<?=$key?>"><?=$value?></a></li>
- <?php
- }
- echo '<br>';
- ?></ul><br>
-
-<!-- <a href="/">Главная</a>
- <a href="/?page=catalog">Каталог</a>
- <a href="/">Корзина ($count)</a><br> -->
+ <?php endforeach;?>
+ </ul><br>
